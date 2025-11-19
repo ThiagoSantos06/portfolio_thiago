4d1ab66f11c28bee733a16514a4916fbf74d82c7
Thiago <thiagohenrique1983c@gmail.com>
2025-11-19 00:07:43 -0300

    💄 style: Adicionar responsividade da intro

--- src/app/pages/intro/intro.ts
+++ src/app/pages/intro/intro.ts
@@ -24,7 +24,7 @@ export class Intro implements AfterViewInit {
   scrollTo(link: string) {
     const section = document.getElementById(link);
     if (section) {
-      section.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
 
